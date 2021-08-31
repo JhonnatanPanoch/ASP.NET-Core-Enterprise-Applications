7c6890aa9e372d7921489ef67c18d3e0e6b4850b
JhonnatanPanoch <43388041+JhonnatanPanoch@users.noreply.github.com>
2021-08-30 23:03:20 -0300

    Adicção projeto mvc de exemplo

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,16 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using NSE.WebApp.MVC.Models;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+        [HttpGet("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
 
+        [HttpPost("nova-conta")]
+        public IActionResult Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid)
+                return View(usuarioRegistro);
+
+
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid)
+                return View(usuarioLogin);
+
+
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("logout")]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
